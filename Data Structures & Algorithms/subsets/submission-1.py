463daf6d7560de47238e16bbf7f7b81dec81778f
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-07 15:17:54 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        output = [[]]
+
+        def recurse(i, curr):
+            nonlocal output
+
+            if i == len(nums):
+                return
+            
+            for j in range(i, len(nums)):
+                curr.append(nums[j])
+                recurse(j + 1, curr)
+                output.append(curr.copy())
+                curr.pop()
+
+        recurse(0, [])
+        return output
