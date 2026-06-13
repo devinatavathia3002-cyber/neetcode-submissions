b12d2cc5cc875b145cb06ce82ce711a5f886e984
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-13 16:48:08 -0700

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        pre = defaultdict(list)
+        visited = set()
+        for pair in prerequisites:
+            past, post = pair
+            pre[past].append(post)
+        
+        def dfs(num):
+            nonlocal visited
+            if pre[num] == []:
+                return True
+            if num in visited:
+                return False
+            
+            visited.add(num)
+            for prereq in pre[num]:
+                if not dfs(prereq):
+                    return False
+            
+            visited.remove(num)
+            pre[num] = []
+            return True
+            
+        for num in range(numCourses):
+            if not dfs(num):
+                return False
+        return True
