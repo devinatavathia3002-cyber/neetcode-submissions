7e715477d75062b8807f8b5e2c6b4411e0b7589f
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-15 23:50:57 -0700

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.py
@@ -0,0 +1,47 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        # recursive approach
+        # def findTotal(i):
+        #     if i >= len(s):
+        #         return 1
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     if i + 1 < len(s) and int(s[i:i+2]) <= 26:
+        #         return findTotal(i + 1) + findTotal(i + 2)
+        #     else:
+        #         return findTotal(i + 1)
+
+        # return findTotal(0)
+
+        # dp solution
+        # dp = [0] * len(s)
+        # dp[0] = 1 if int(s[0]) != 0 else 0
+        
+        # for i in range(1, len(s)):
+        #     both = int(s[i - 1: i + 1])
+        #     single = int(s[i])
+
+        #     if single != 0:
+        #         dp[i] += dp[i-1]
+        #     if 10 <= both <= 26:
+        #         dp[i] += dp[i-2] if i >= 2 else 1
+
+        # return dp[len(s) - 1]
+
+        # another dp solution
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                total = dp[i + 1]
+                total += dp[i + 2] if (i + 2) <= len(s) and int(s[i:i+2]) <= 26 else 0
+                dp[i] = total
+        
+        return dp[0]
+
+        # [2, 0, 2, 1, 1]
